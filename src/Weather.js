0a514fdb6acc03abc70fbc081950f5f6f2eb4211
irism1994 <iris.magdelyns@gmail.com>
2020-11-14 15:41:57 +0100

    Updated css Weather

--- src/Weather.js
+++ src/Weather.js
@@ -5,34 +5,40 @@ export default function Weather(){
     return(
 
     <div className ="Weather"> 
-    
-    <form>
-        <div className ="row">
-            <div className="col-9">
-                <input
-                type ="search"
-                placeholder ="Search for city..."
-                className = "form-control" 
-                autoFocus ="on"
-                />
-            </div>
-            <div className ="col-3">
-                <input 
-                type ="submit"
-                value ="Search"
-                className = "btn btn primary" />
+        <div class="input-group">
+            <input 
+                type="text" 
+                class="form-control" 
+                placeholder="Enter a city"
+                aria-label="Recipient's username with two button addons" 
+                aria-describedby="button-addon4"
+                id="search-city-input" 
+            />
+            <div class="input-group-append" id="button-addon4">
+                <button 
+                    class="btn btn-outline-secondary" 
+                    type="button"
+                    id="search-city">
+                        Search
+                </button>
+                <button 
+                    class="btn btn-outline-secondary" 
+                    type="button"
+                    id="current-city">
+                        Current place
+                </button>
             </div>
         </div>
-    </form>
 
-    <h1>Heemskerk</h1>
-    <ul>
-        <li>zaterdag 14.00</li>
-        <li>Bewolkt</li>
-    </ul>
+        <h1>Heemskerk</h1>
+        <ul>
+            <li>zaterdag 14.00</li>
+            <li>Bewolkt</li>
+        </ul>
     
         <div className= "row">
             <div className="col-6 temperature">
+
                 <img src ="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt ="bewolkt" />
                 13 °C | °F
             </div>
